4643f379185896c043b6d18267cfd3f14436e4e0
darina smirnova <darismi01@yandex.ru>
2023-12-04 13:53:38 +0300

    добавлена функция upper_first_letter и докстринг к функции func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 def func(str):
+    ''' Делает все буквы заглавными '''
     upper_str = str.upper()
     print(upper_str)
+
+def upper_first_letter(str):
+    ''' Делает заглавными первые буквы каждого слова в строке '''
+    upper_str = str.title()
+    print(upper_str)
+
